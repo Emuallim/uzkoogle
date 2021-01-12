3b0719a32c6cb7dc3e5f82339e200f490910ba2b
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-01-13 00:07:03 +0500

    add react-router & use router, switch, route

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import Search from './pages/Search';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>App Header here</h1>
-      {/* Home (the one with the search on) */}
-      <Home />
-      {/* SearchPage (the results page) */}
-      <Search />
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is the search page</h1>
+            <Search />
+          </Route>
+          <Route path="/">
+            <h1>This is the home page</h1>
+            <Home />
+          </Route>
+        </Switch>
+        <h1>App Header here</h1>
+
+      </Router>
     </div>
   );
 }
