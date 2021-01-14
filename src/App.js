7c1db320b2c19876a783210913999e9aaffd621c
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-01-14 11:35:59 +0500

    style the home header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home';
-import Search from './pages/Search';
+import Home from './Home';
+import Search from './Search';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -10,16 +10,12 @@ function App() {
       <Router>
         <Switch>
           <Route path="/search">
-            <h1>This is the search page</h1>
             <Search />
           </Route>
           <Route path="/">
-            <h1>This is the home page</h1>
             <Home />
           </Route>
         </Switch>
-        <h1>App Header here</h1>
-
       </Router>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import { Avatar } from '@material-ui/core';
 // we couda used <a> but it refreshes the page
 // but this does it w/o refresh
 
 function Home() {
   return (
     <div className="home">
-      <h1>This is HOMEPAGE</h1>
       <div className="home_header">
         <div className="home_headerLeft">
           <Link to='/about'>About</Link>
@@ -17,7 +18,8 @@ function Home() {
         <div className="home_headerRight">
           <Link to='/gmail'>Gmail</Link>
           <Link to='/images'>Images</Link>
-          {/* Icon */}
+          <AppsIcon className="apps" />
+          <Avatar />
           {/* Avatar */}
         </div>
       </div>
