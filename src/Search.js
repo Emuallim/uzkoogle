01bde43a1471ded6766bf7c7d636e895a55f0e27
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-01-15 22:35:07 +0500

    change search div to form and google search btn type to submit

--- src/Search.js
+++ src/Search.js
@@ -7,21 +7,32 @@ import { Button } from "@material-ui/core";
 function Search() {
   // state = variables in react
   const [input, setInput] = useState('');
+  // whatever you type in the search, goes into "input"
   const search = e => {
-    e.preventDefault();
+    e.preventDefault(); //stops page refresh
+    // how to trigger the "Google Search" key when you hit enter
+    console.log("You Entered >>", input);
   }
+
+  // document.addEventListener("keyup", (e) => {
+  //   if (e.keyCode == 13) {
+  //     console.log("you hit Enter");
+  //   }
+  // })
+
   return (
-    <div className="search">
+    <form className="search">
       <div className="search_input">
         <SearchIcon className="search_inputIcon" />
         <input value={input} onChange={e => setInput(e.target.value)} />
+        {/* whatever you type in is captured in the state above */}
         <MicIcon />
       </div>
       <div className="search_btns">
-        <Button onClick={search} variant="outlined">Google Search</Button>
+        <Button type='submit' onClick={search} variant="outlined">Google Search</Button>
         <Button onClick={search} variant="outlined">I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   )
 }
 
