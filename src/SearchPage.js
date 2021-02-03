788c14f9458dc259ac73eb158219f1b748ca1926
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-02-04 01:33:56 +0500

    grid magic worked

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -27,95 +27,89 @@ function SearchPage() {
   // console.log(data);
   return (
     <div className='searchPage'>
-      <div className='searchPage_header'>
-        {/* TOP SECTION -------------------- */}
-        <div className='searchPage_headerTop'>
-          <Link to="/">
-            <img className="searchPage_logo"
-              src={process.env.PUBLIC_URL + '/googlemini.png'} />
-          </Link>
-          <div className="searchPage_input">
-            <Search hideButtons />
+      {/* TOP THREE -------------------- */}
+      <Link to="/">
+        <img className="searchPage_logo"
+          src={process.env.PUBLIC_URL + '/googlemini.png'} />
+      </Link>
+      <div className="searchPage_search">
+        <Search hideButtons />
+      </div>
+      <div className='searchPage_login'>
+        <AppsIcon className="appsIcon" />
+        <Avatar />
+      </div>
+
+
+      {/* OPTIONS -------------------- */}
+      <div className="searchPage_options">
+        <div className="searchPage_optionsLeft">
+          <div className="searchPage_option">
+            <SearchIcon />
+            <Link to="/all">All</Link>
           </div>
-          <div className='searchPage_login'>
-            <AppsIcon className="appsIcon" />
-            <Avatar />
+          <div className="searchPage_option">
+            <ImageIcon />
+            <Link to="/images">Images</Link>
+          </div>
+          <div className="searchPage_option">
+            <DescriptionIcon />
+            <Link to="/news">News</Link>
+          </div>
+          <div className="searchPage_option">
+            <PlayCircleFilledWhite />
+            <Link to="/videos">Videos</Link>
+          </div>
+
+          <div className="searchPage_option">
+            <MoreVertIcon />
+            <Link to="/more">More</Link>
           </div>
         </div>
 
-        {/* BODY SECTION -------------------- */}
-        <div className='searchPage_headerBody'>
-          <div className="searchPage_options">
-            <div className="searchPage_optionsLeft">
-              <div className="searchPage_option">
-                <SearchIcon />
-                <Link to="/all">All</Link>
-              </div>
-              <div className="searchPage_option">
-                <ImageIcon />
-                <Link to="/images">Images</Link>
-              </div>
-              <div className="searchPage_option">
-                <DescriptionIcon />
-                <Link to="/news">News</Link>
-              </div>
-              <div className="searchPage_option">
-                <PlayCircleFilledWhite />
-                <Link to="/videos">Videos</Link>
-              </div>
 
-              <div className="searchPage_option">
-                <MoreVertIcon />
-                <Link to="/more">More</Link>
-              </div>
-            </div>
-
-
-            <div className="searchPage_optionsRight">
-              <div className="searchPage_option">
-                <Link to="/settings">Settings</Link>
-              </div>
-              <div className="searchPage_option">
-                <Link to="/tool">Tools</Link>
-              </div>
-            </div>
+        <div className="searchPage_optionsRight">
+          <div className="searchPage_option">
+            <Link to="/settings">Settings</Link>
+          </div>
+          <div className="searchPage_option">
+            <Link to="/tool">Tools</Link>
           </div>
         </div>
       </div>
 
-      <div className='searchPage_resultsContainer'>
-        {term && (
-          // render the results ONLY if there is search term
-          <div className='searchPage_results'>
-            <p className="searchPage_resultsCount">
-              {/* About 3000000 results (0.3 seconds) for tesla */}
+      {/* RESULTS -------------------------- */}
+      {term && (
+        // render the results ONLY if there is search term
+        <div className='searchPage_results'>
+          <p className="searchPage_resultsCount">
+            {/* About 3000000 results (0.3 seconds) for tesla */}
           About {data?.searchInformation.formattedTotalResults} results ({data?.searchInformation.formattedSearchTime} seconds) for {term}
-            </p>
+          </p>
 
-            {data?.items.map(item => (
-              <div className='searchPage_result'>
-                <a href={item.link}>
-                  {item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src && (
-                    <img className="searchPage_resultImage"
-                      src={
-                        item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src
-                      } alt="" />
-                  )}
-                  {item.displayLink}
-                </a>
-                <a className='searchPage_resultTitle'
-                  href={item.link}>
-                  <h2>{item.title}</h2>
-                </a>
-                <p className='searchPage_resultSnippet'>
-                  {item.snippet}
-                </p>
-              </div>
-            ))}
-          </div>
-        )}
-        <div className='searchPage_resultsContainer-right'>right border</div>
-      </div>
+          {data?.items.map(item => (
+            <div className='searchPage_result'>
+              <a href={item.link}>
+                {item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src && (
+                  <img className="searchPage_resultImage"
+                    src={
+                      item.pagemap?.cse_image?.length > 0 && item.pagemap?.cse_image[0]?.src
+                    } alt="" />
+                )}
+                {item.displayLink}
+              </a>
+              <a className='searchPage_resultTitle'
+                href={item.link}>
+                <h2>{item.title}</h2>
+              </a>
+              <p className='searchPage_resultSnippet'>
+                {item.snippet}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+
       <div className="searchPage_footer">
         <h1>Footer</h1>
       </div>
