c2aa3b14362aa50f4ff49a6d2b6500bab6392535
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-02-24 00:51:43 +0500

    get user value from input and save it to variable

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -4,23 +4,10 @@ import "./Help.css";
 import Navbar from '../Navbar';
 
 function Help() {
-  /*
-   * Guess the Number 
-   * TODO: Get user value from input and save it to variable numberGuess
-   * TODO: Generate a random number 1 to 100 and save it to variable correctNumber
-   * TODO: Console whether the guess is too high/low or is correct inside playGame
-   * TODO: Create a function called displayResult to move the logic for if the guess
-   * TODO: Complete the showYouWon, showNumAbove, showNumBelow functions
-   * TODO: Use the showYouWon... functions within displayResult
-   * TODO: Save the guess history in a variable called guuess
-   * TODO: Display the guess history using displayHistory() function
-   * TODO: Use the initGame() function to restart the game
-   */
   // Variable to store the list of guesses
-  let guesses = [];
 
   // Variable to store the correct random number
-  let correctNumber = [];
+
 
   let textInput = React.createRef();
 
@@ -38,7 +25,7 @@ function Help() {
         <div>
           <input type="text" ref={textInput} />
           <button onClick={showMsg}>Click</button>
-          <button onClick={showMsg}>Restart</button>
+          {/* <button onClick={showMsg}>Restart</button> */}
         </div>
       </div>
     </div>
---
+++ src/pages/Todos.md
@@ -0,0 +1,29 @@
+# ToDoList - Guess the Number Game
+
+A basic todo list built to demonstrate a frontend app (only client)
+
+with the following features:
+
+- show the result for if the guess is too high/low or correct
+- HINT: Use if, else if, else statment
+
+## TODO
+
+- [ ] ⌨️ Get user value from input and save it to variable numberGuess
+- [ ] ➡️ Generate a random number 1 to 100 and save it to variable correctNumber
+- [ ] 🗃 Console whether the guess is too high/low or is correct inside playGame
+- [ ] 🔍 Create a function called displayResult to move the logic for if the guess
+- [ ] 🙈 Complete the showYouWon, showNumAbove, showNumBelow functions
+- [ ] ⌨️ Save the guess history in a variable called guuess
+- [ ] 🗃 Display the guess history using displayHistory() function
+- [ ] 🔍 Use the initGame() function to restart the game
+
+
+## What's next?
+
+- Add time & date to item
+- Add deadline to item
+- Search for item(s)
+- User Sign up/Login
+- User Profiles
+  - Only show items from given user
