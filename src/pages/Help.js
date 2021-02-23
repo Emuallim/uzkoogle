cc2cf2c09d183da292323ac543f042156e3c587e
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-02-23 23:48:47 +0500

    make a todo list for Guess the Number

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import "./Pages.css";
+import "./Help.css";
 import Navbar from '../Navbar';
 
 function Help() {
+  /*
+   * Guess the Number 
+   * TODO: Get user value from input and save it to variable numberGuess
+   * TODO: Generate a random number 1 to 100 and save it to variable correctNumber
+   * TODO: Console whether the guess is too high/low or is correct inside playGame
+   * TODO: Create a function called displayResult to move the logic for if the guess
+   * TODO: Complete the showYouWon, showNumAbove, showNumBelow functions
+   * TODO: Use the showYouWon... functions within displayResult
+   * TODO: Save the guess history in a variable called guuess
+   * TODO: Display the guess history using displayHistory() function
+   * TODO: Use the initGame() function to restart the game
+   */
+  // Variable to store the list of guesses
+  let guesses = [];
+
+  // Variable to store the correct random number
+  let correctNumber = [];
+
+  let textInput = React.createRef();
+
   const showMsg = () => {
-    document.querySelector('h1').innerHTML = "Hello";
+    document.querySelector('h1').innerHTML = textInput.current.value;
   }
 
   return (
@@ -12,12 +33,19 @@ function Help() {
       <div className="wrapper_navbar">
         <Navbar />
       </div>
-      <div className="wrapper_body">
+      <div className="wrapper_body guess-number">
         <h1></h1>
-        <input type="text" /><button onClick={showMsg}>Click</button>
+        <div>
+          <input type="text" ref={textInput} />
+          <button onClick={showMsg}>Click</button>
+          <button onClick={showMsg}>Restart</button>
+        </div>
       </div>
     </div>
   )
+
+
+
 }
 
 export default Help
