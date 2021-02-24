d8c6fbd4d4b302639f2e9f3ec126051206e2cbfe
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-02-25 02:12:52 +0500

    display in h1 whether the guess is high/low/correct inside playGame

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -6,19 +6,18 @@ import Navbar from '../Navbar';
 function Help() {
   // Variable to store the list of guesses
 
-  // Variable to store the correct random number
-
   let textInput = React.createRef();
+  let randomNumber = Math.floor(Math.random() * 101);
 
   const playGame = () => {
     let numberGuess = textInput.current.value;
-    document.querySelector('h1').innerHTML = numberGuess;
-  }
-
-  const getRandomNumber = () => {
-    let randomNumber = Math.floor(Math.random() * 101);
-    document.querySelector('h2').innerHTML = randomNumber;
-    return randomNumber;
+    if (numberGuess > randomNumber) {
+      document.querySelector('h1').innerHTML = "too high";
+    } else if (numberGuess < randomNumber) {
+      document.querySelector('h1').innerHTML = "too low";
+    } else {
+      document.querySelector('h1').innerHTML = "correct";
+    }
   }
 
   return (
@@ -27,11 +26,10 @@ function Help() {
         <Navbar />
       </div>
       <div className="wrapper_body game-board">
-        <h1></h1><h2></h2>
+        <h1></h1>
         <div>
-          <input type="text" ref={textInput} />
+          <input type="number" max='100' ref={textInput} />
           <button onClick={playGame}>Click</button>
-          <button onClick={getRandomNumber}>Random</button>
         </div>
       </div>
     </div>
