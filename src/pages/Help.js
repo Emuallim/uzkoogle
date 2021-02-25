8a90773b8d8cee60fadbb2b33d6ab12c6d7c63dc
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-02-26 00:10:50 +0500

    create showYouWon function that displays messages when yes/no

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -24,6 +24,26 @@ function Help() {
     }
   }
 
+  const getDialog = (dialogType, text) => {
+    let dialog;
+    switch (dialogType) {
+      case "warning":
+        dialog = "<div class='alert alert-warning' role='alert'>";
+        break;
+      case "won":
+        dialog = "<div class='alert alert-success' role='alert'>";
+        break;
+    }
+    dialog += text;
+    dialog += "</div>"
+    return dialog;
+  }
+
+  const showYouWon = (won) => {
+    let dialog = getDialog(won);
+    document.querySelector("#result").innerHTML = dialog;
+  }
+
   return (
     <div className="wrapper">
       <div className="wrapper_navbar">
@@ -35,6 +55,7 @@ function Help() {
           <input type="number" max='100' ref={textInput} />
           <button onClick={playGame}>Click</button>
         </div>
+        <div id="result"></div>
       </div>
     </div>
   )
