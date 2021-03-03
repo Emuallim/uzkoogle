104dc8f811b689dbd5700b136495b0a60fbf484b
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-03-03 18:11:54 +0500

    alert onclick if there is no value in input

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -9,16 +9,22 @@ function Help() {
   let textInput = React.createRef();
   let randomNumber = Math.floor(Math.random() * 101);
 
-
-  const playGame = () => {
+  const playGame = e => {
+    e.preventDefault();
     let numberGuess = textInput.current.value;
     displayResult(numberGuess);
     saveGuessHistory(numberGuess);
     console.log(randomNumber);
   }
 
+  const restart = () => {
+    document.querySelector('#history').innerHTML = "";
+  }
+
   const displayResult = (numberGuess) => {
-    if (numberGuess > randomNumber) {
+    if (numberGuess == "") {
+      alert('Please enter valid number');
+    } else if (numberGuess > randomNumber) {
       showNumberAbove();
     } else if (numberGuess < randomNumber) {
       showNumberBelow();
@@ -64,11 +70,15 @@ function Help() {
   }
 
   const saveGuessHistory = (numberGuess) => {
-    guesses.push(numberGuess);
-    displayHistory(guesses);
+    if (numberGuess == "") {
+      alert('Please enter valid number');
+    } else {
+      guesses.push(numberGuess);
+      displayHistory(guesses);
+    }
   }
 
-  const displayHistory = () => {
+  const displayHistory = (guesses) => {
     let list = "<ul class='list'>"
     for (let i = 0; i < guesses.length; i++) {
       list += `<li class='list-item'>You guessed ${guesses[i]}</li>`
@@ -78,21 +88,21 @@ function Help() {
   }
 
   return (
-    <div className="wrapper">
+    <form className="wrapper">
       <div className="wrapper_navbar">
         <Navbar />
       </div>
       <div className="wrapper_body game-board">
         <h2>Guess the Number!</h2>
-        <div>
-          <input type="number" max='100' ref={textInput} />
-          <button onClick={playGame}>Click</button>
+        <div className='input_container'>
+          <input type="number" max='100' ref={textInput} placeholder="press enter" />
+          <button onClick={playGame} hidden>Restart</button>
+          <button onClick={restart}>Restart</button>
         </div>
         <div id="result"></div>
-        <div id="history">
-        </div>
+        <div id="history"></div>
       </div>
-    </div>
+    </form>
   )
 }
 
