4d536395187361a835fd4e9f1f74f33975940dea
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-03-06 00:28:36 +0500

    minutes to seconds

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -4,17 +4,23 @@ import Navbar from '../Navbar';
 
 function Feedback() {
   const addNumbers = (num1, num2) => {
-    let result = num1 + num2;
-    return result;
+    return num1 + num2;
   }
-  console.log(addNumbers(5, 2));
+
+  const minuteToSecond = () => {
+    let input = prompt('enter number');
+    let result = input * 60;
+    document.querySelector('h2').innerHTML = `${input} minute(s) = ${result} seconds`;
+  }
+
+
   return (
     <div className="wrapper">
       <div className="wrapper_navbar">
         <Navbar />
       </div>
       <div className="wrapper_body">
-        <h2>Feedback Here!</h2>
+        <h2 onClick={minuteToSecond}>Click Here!</h2>
       </div>
     </div>
   )
